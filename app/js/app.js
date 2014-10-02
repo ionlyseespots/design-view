8865f14fda68b079f7c8ace68d6e2baf7bc4dd4b
ionlyseespots <kburas@four51.com>
2014-10-02 16:09:29 -0500

    sandboxing

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 var Simple = {};
 
-Simple.app = angular.module('SimpleApp',['ngRoute', 'colorpicker.module', 'FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap']).
+Simple.app = angular.module('SimpleApp',['ngRoute', 'ngAnimate','FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/colors.html', controller: 'colorThemeCtrl'});
         $routeProvider.when('/homepage', {templateUrl: 'partials/homePage.html', controller: 'colorThemeCtrl'});
--- app/js/controllers/SimpleCtrl.js
+++ app/js/controllers/SimpleCtrl.js
@@ -10,4 +10,16 @@ Simple.app.controller("SimpleCtrl", function($scope, Fullscreen){
         // Fullscreen.enable( document.getElementById('img') )
 
     }
+
+    $scope.open = function() {
+        $scope.showModal = true;
+    };
+
+    $scope.ok = function() {
+        $scope.showModal = false;
+    };
+
+    $scope.cancel = function() {
+        $scope.showModal = false;
+    };
 });
