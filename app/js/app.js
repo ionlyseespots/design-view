51f3ce5c690356d1a5f1e2f371058f27495d1c3e
ionlyseespots <kburas@four51.com>
2014-10-02 16:14:16 -0500

    sandboxing

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,8 @@ var Simple = {};
 Simple.app = angular.module('SimpleApp',['ngRoute', 'ngAnimate','FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/colors.html', controller: 'colorThemeCtrl'});
+        $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'colorThemeCtrl'});
         $routeProvider.when('/homepage', {templateUrl: 'partials/homePage.html', controller: 'colorThemeCtrl'});
         $routeProvider.when('/backgrounds', {templateUrl: 'partials/backgroundImages.html', controller: 'colorThemeCtrl'});
-//        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.otherwise({redirectTo: '/dashboard'});
     }]);
