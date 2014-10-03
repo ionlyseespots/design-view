66e036e01517e2e87e91dc0bad005b0431a51e4a
ionlyseespots <kburas@four51.com>
2014-10-03 09:47:57 -0500

    sandboxing

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,7 @@
 var Simple = {};
 
-Simple.app = angular.module('SimpleApp',['ngRoute', 'ngAnimate','FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal']).
+Simple.app = angular.module('SimpleApp',['ngRoute', 'FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal']).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/colors.html', controller: 'colorThemeCtrl'});
-        $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'colorThemeCtrl'});
-        $routeProvider.when('/homepage', {templateUrl: 'partials/homePage.html', controller: 'colorThemeCtrl'});
-        $routeProvider.when('/backgrounds', {templateUrl: 'partials/backgroundImages.html', controller: 'colorThemeCtrl'});
-        $routeProvider.otherwise({redirectTo: '/dashboard'});
+        $routeProvider.when('/themeEditor', {templateUrl: 'partials/themeEditor.html', controller: 'colorThemeCtrl'});
+        $routeProvider.otherwise({redirectTo: '/themeEditor'});
     }]);
