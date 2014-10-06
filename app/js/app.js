8a702f3759069bb229d6aa202fddf63683795084
ionlyseespots <kburas@four51.com>
2014-10-06 15:48:17 -0500

    break out new html into own partial

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,6 @@ Simple.app = angular.module('SimpleApp',['ngRoute', 'FBAngular', 'textAngular', 
         $routeProvider.when('/headerTemplate', {templateUrl: 'partials/header.html', controller: 'colorThemeCtrl'});
         $routeProvider.when('/loginBackground', {templateUrl: 'partials/loginBackground.html', controller: 'colorThemeCtrl'});
         $routeProvider.when('/bodyBackground', {templateUrl: 'partials/bodyBackground.html', controller: 'colorThemeCtrl'});
+        $routeProvider.when('/newTemplate', {templateUrl: 'partials/newTemplate.html', controller: 'colorThemeCtrl'});
         $routeProvider.otherwise({redirectTo: '/headerTemplate'});
     }]);
